f9c66c8ac5aabe7d3e746b28bbac7d3c9fabda45
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-20 18:20:05 -0500

    Create main endpoints

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ import express from 'express'
 
 const app = express()
 
+app.get('/employees', (req, res) => {res.send('Get employees')})
+
+app.post('/employees', (req, res) => {res.send('Post employees')})
+
+app.put('/employees', (req, res) => {res.send('Put employees')})
+
+app.delete('/employees', (req, res) => {res.send('Delete employees')})
+
 app.listen(3000)
 console.log("Server listening on port 3000")
