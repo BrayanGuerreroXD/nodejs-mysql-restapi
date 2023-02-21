0d2d9eaa2a2da7b87a20934cb615721c8450b223
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-21 12:04:16 -0500

    Create routes files

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import express from "express";
-import { pool } from "./db.js";
+import { employeesRoutes } from "./routes/employees.routes.js";
+import { indexRoutes } from "./routes/index.routes.js";
 
 const app = express();
 
-app.get("/ping", async (req, res) => {
-  const [result] = await pool.query("SELECT 1 + 1 AS Result;");
-  res.json(result);
-});
-
-app.get("/employees", (req, res) => {
-  res.send("Get employees");
-});
-
-app.post("/employees", (req, res) => {
-  res.send("Post employees");
-});
-
-app.put("/employees", (req, res) => {
-  res.send("Put employees");
-});
-
-app.delete("/employees", (req, res) => {
-  res.send("Delete employees");
-});
+app.use(indexRoutes);
+app.use(employeesRoutes);
 
 app.listen(3000);
 console.log("Server listening on port 3000");
---
+++ src/routes/employees.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/employees", (req, res) => {
+  res.send("Get employees");
+});
+
+router.post("/employees", (req, res) => {
+  res.send("Post employees");
+});
+
+router.put("/employees", (req, res) => {
+  res.send("Put employees");
+});
+
+router.delete("/employees", (req, res) => {
+  res.send("Delete employees");
+});
+
+export default router;
