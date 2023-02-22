e84cd83bdadb8953a8ead7a87515986dffc172fb
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-21 22:12:13 -0500

    Add not found route

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ import indexRoutes from "./routes/index.routes.js";
 const app = express();
 
 app.use(express.json())
+
 app.use(indexRoutes);
 app.use('/api/', employeesRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Not found"
+    })
+})
 
 app.listen(3000);
 console.log("Server listening on port 3000");
